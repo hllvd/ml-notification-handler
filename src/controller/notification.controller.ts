779fd2ebf668963a8896974d82d12bdfbc5dfe70
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-07-26 20:32:26 -0300

    send message to sqs

--- src/controller/notification.controller.ts
+++ src/controller/notification.controller.ts
@@ -10,39 +10,39 @@ const receiver = async (req, res) => {
   if (req.query.token !== "2d2aedde-728c-473c-a1a2-cfaef52057f4")
     res.json({ error: "Invalid token" })
 
-  // console.log("Before variables")
-  // const created = new Date().toISOString().slice(0, 19).replace("T", " ")
-  // const body = req.body
-  // const payload = { ...body, created }
-  // const payloadStr = JSON.stringify(payload)
-  // const payloadSerialized = payloadStr
-  // const sqsClient = new SQSClient({
-  //   region: "sa-east-1",
-  //   credentials: {
-  //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  //     secretAccessKey: process.env.AWS_ACESS_KEY,
-  //   },
-  // })
+  console.log("Before variables")
+  const created = new Date().toISOString().slice(0, 19).replace("T", " ")
+  const body = req.body
+  const payload = { ...body, created }
+  const payloadStr = JSON.stringify(payload)
+  const payloadSerialized = payloadStr
+  const sqsClient = new SQSClient({
+    region: "sa-east-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_ACCESS_KEY,
+    },
+  })
 
-  // console.log("Before file")
-  // const homeDir = process.env.HOME || process.env.USERPROFILE
-  // console.log("Before file 2")
-  // const outputFilePath = path.join(homeDir, "post.input.txt")
-  // console.log("Before file 3")
-  // fs.appendFileSync(outputFilePath, payloadStr)
-  // fs.appendFileSync(outputFilePath, ` ----------------- \n\n`)
-  // console.log("Before input")
-  // const input = {
-  //   QueueUrl: process.env.QUEUE_URL,
-  //   MessageBody: payloadSerialized,
-  // }
-  // console.log("Preparing command")
-  // const command = new SendMessageCommand(input)
+  console.log("Before file")
+  const homeDir = process.env.HOME || process.env.USERPROFILE
+  console.log("Before file 2")
+  const outputFilePath = path.join(homeDir, "post.input.txt")
+  console.log("Before file 3")
+  fs.appendFileSync(outputFilePath, payloadStr)
+  fs.appendFileSync(outputFilePath, ` ----------------- \n\n`)
+  console.log("Before input")
+  const input = {
+    QueueUrl: process.env.QUEUE_URL,
+    MessageBody: payloadSerialized,
+  }
+  console.log("Preparing command")
+  const command = new SendMessageCommand(input)
 
   try {
     console.log("NO Sending message")
-    //await sqsClient.send(command)
-
+    await sqsClient.send(command)
+    console.log("msg sent")
     /**
      * # Hack context
      * While ML response should be under 500ms, we need to wait for
