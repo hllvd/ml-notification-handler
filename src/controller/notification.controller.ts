9e99ec1d1b6301c1ca29c944d410e799e9bdeda1
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-07-26 01:52:29 -0300

    add console logs

--- src/controller/notification.controller.ts
+++ src/controller/notification.controller.ts
@@ -10,17 +10,22 @@ const receiver = async (req, res) => {
   if (req.query.token !== "2d2aedde-728c-473c-a1a2-cfaef52057f4")
     res.json({ error: "Invalid token" })
 
+  console.log("Before variables")
   const created = new Date().toISOString().slice(0, 19).replace("T", " ")
   const body = req.body
   const payload = { ...body, created }
-  const payloadSerialized = JSON.stringify(payload)
+  const payloadStr = JSON.stringify(payload)
+  const payloadSerialized = payloadStr
   const sqsClient = new SQSClient({ region: "sa-east-1" })
 
+  console.log("Before file")
   const homeDir = process.env.HOME || process.env.USERPROFILE
+  console.log("Before file 2")
   const outputFilePath = path.join(homeDir, "post.input.txt")
-  fs.appendFileSync(outputFilePath, body)
+  console.log("Before file 3")
+  fs.appendFileSync(outputFilePath, payloadStr)
   fs.appendFileSync(outputFilePath, ` ----------------- \n\n`)
-
+  console.log("Before input")
   const input = {
     QueueUrl: process.env.QUEUE_URL,
     MessageBody: payloadSerialized,
@@ -39,7 +44,7 @@ const receiver = async (req, res) => {
      * In other words, performance here is important
      */
     setTimeout(async () => {
-      await notificationService.processMessages()
+      //await notificationService.processMessages()
     }, 10000)
 
     res.json({})
