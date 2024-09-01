ddd699bbbad5224c98be7652e3f3288ed5a43cb4
Hudson leandro Leal van-dal <hllvd@hotmail.com>
2024-09-01 16:55:49 -0300

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "ml-notification-with-health-checker",
+      script: "npm",
+      args: "run start",
+      cmd: "/home/mercadolivre/ml-notification-handler",
+      watch: true,
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "production",
+      },
+      health_check: {
+        http_url: "http://localhost:3000",
+        interval: 600000,
+        max_retries: 2,
+      },
+    },
+  ],
+}
